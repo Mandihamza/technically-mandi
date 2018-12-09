f93d386e6bfe8b6dfca72be52471067e704237c7
mandihamza <mandihamza@gmail.com>
2018-12-09 13:57:38 -0700

    Initial portfolio edits

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -130,7 +130,7 @@ const Index = () => (
     <Parallax pages={5}>
       <Divider speed={0.2} offset={0}>
         <UpDown>
-          <SVG icon="triangle" className={hidden} width={48} stroke={colors.orange} left="10%" top="20%" />
+          <SVG icon="triangle" className={hidden} width={48} stroke={colors.pink} left="10%" top="20%" />
           <SVG icon="hexa" width={48} stroke={colors.red} left="60%" top="70%" />
           <SVG icon="box" width={6} fill={colors['grey-darker']} left="60%" top="15%" />
         </UpDown>
@@ -158,9 +158,9 @@ const Index = () => (
       <Content speed={0.4} offset={0}>
         <Hero>
           <BigTitle>
-            Hello, <br /> I'm John Doe.
+            Hello, <br /> I'm Mandi Hamza.
           </BigTitle>
-          <Subtitle>I'm creating noice web experiences for the next generation of consumer-facing companies.</Subtitle>
+          <Subtitle>I'm a software developer in training with experience in e-commerce marketing & sponsorship development. <br /><br />My favorite projects live here. Take a look!</Subtitle>
         </Hero>
       </Content>
       <DividerMiddle
@@ -174,32 +174,34 @@ const Index = () => (
           <Title>Projects</Title>
           <ProjectsWrapper>
             <ProjectCard
-              title="Freiheit"
-              link="https://www.behance.net/gallery/58937147/Freiheit"
+              title="Neil DeGrasse Tyson: A tribute."
+              link="https://codepen.io/mandihamza/pen/ALXoRA"
               bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
             >
-              This project is my entry to Adobe's #ChallengeYourPerspective contest.
+              This project was created as part of the freeCodeCamp curriculum.
             </ProjectCard>
             <ProjectCard
-              title="Harry Potter"
-              link="https://www.behance.net/gallery/52915793/Harry-Potter"
+              title="Her Many Voices Foundation"
+              link="https://www.hermanyvoices.org/"
               bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)"
             >
-              I entered the DOCMA 2017 award with this Harry Potter inspired image.
+              A website I built, and maintain for a local women's Non-Profit.
             </ProjectCard>
             <ProjectCard
-              title="Tomb Raider"
-              link="https://www.behance.net/gallery/43907099/Tomb-Raider"
+              title="Haiku Generator"
+              link="https://www.#.com"
               bg="linear-gradient(to right, #009245 0%, #FCEE21 100%)"
             >
-              Recreation of a Tomb Raider Wallpaper (Fan Art)
+            Click here and enjoy<br />
+            Tweet sized Haiku beauties<br />
+            Digital poetry
             </ProjectCard>
             <ProjectCard
-              title="Eagle"
-              link="https://www.behance.net/gallery/38068151/Eagle"
+              title="[WIP] Blog"
+              link="#"
               bg="linear-gradient(to right, #D585FF 0%, #00FFEE 100%)"
             >
-              A fantasy image manipulation relocating the habitat of wild animals.
+              A blog to share my experiences as I become a software developer.
             </ProjectCard>
           </ProjectsWrapper>
         </Inner>
@@ -232,7 +234,7 @@ const Index = () => (
           <SVG icon="box" className={hidden} width={6} fill={colors.blue} left="50%" top="75%" />
           <SVG icon="upDown" className={hidden} width={8} fill={colors['grey-darkest']} left="70%" top="20%" />
           <SVG icon="triangle" width={8} stroke={colors['grey-darkest']} left="25%" top="5%" />
-          <SVG icon="upDown" className={hidden} width={24} fill={colors.orange} left="80%" top="80%" />
+          <SVG icon="upDown" className={hidden} width={24} fill={colors.pink} left="80%" top="80%" />
         </UpDown>
         <UpDownWide>
           <SVG icon="arrowUp" className={hidden} width={16} fill={colors.purple} left="5%" top="80%" />
@@ -252,16 +254,11 @@ const Index = () => (
           <AboutHero>
             <Avatar src={avatar} alt="John Doe" />
             <AboutSub>
-              The English language can not fully capture the depth and complexity of my thoughts. So I'm incorporating
-              Emoji into my speech to better express myself. Winky face.
+              I'm a self-taught developer with experience in e-commerce marketing & sponsorship development.
             </AboutSub>
           </AboutHero>
           <AboutDesc>
-            You know the way you feel when you see a picture of two otters holding hands? That's how you're gonna feel
-            every day. My mother cried the day I was born because she knew she’d never be prettier than me. You should
-            make me your campaign manager. I was born for politics. I have great hair and I love lying. Captain? The
-            kids want to know where Paulie the Pigeon is. I told them he got sucked up into an airplane engine, is that
-            all right?
+            You know the feeling you get the first time you print 'Hello World'? That's a feeling I live for everyday. I love helping organizations build web experiences that deliver that same magical feeling.
           </AboutDesc>
         </Inner>
       </Content>
@@ -285,14 +282,14 @@ const Index = () => (
         <Inner>
           <Title>Get in touch</Title>
           <ContactText>
-            Say <a href="mailto:plizNoSp4m@domain.tld">Hi</a> or find me on other platforms:{' '}
-            <a href="https://dribbble.com/LekoArts">Dribbble</a> &{' '}
-            <a href="https://www.instagram.com/lekoarts.de/">Instagram</a>
+            Say <a href="https://docs.google.com/forms/d/e/1FAIpQLSeADbrbc0Vce-z5hy81lYQSDhghZzXzAqyXQXZgTbcg1H-0MA/viewform?usp=sf_link">Hi</a> or find me on other platforms:{' '}
+            <a href="https://github.com/Mandihamza">GitHub</a> &{' '}
+            <a href="https://twitter.com/mandihamza/">Twitter</a>
           </ContactText>
         </Inner>
         <Footer>
-          &copy; 2018 by Gatsby Starter Portfolio Cara.{' '}
-          <a href="https://github.com/LekoArts/gatsby-starter-portfolio-cara">Github Repository</a>.
+          &copy; 2018 Built with Gatsby Starter Portfolio Cara.{' '}
+          <a href="https://github.com/Mandihamza/technically-mandi">Github Repository</a>.
         </Footer>
       </Content>
       <Divider speed={0.1} offset={4}>
