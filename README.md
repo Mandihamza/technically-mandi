a992e607a8015d912bccd2216cf507e3401398e9
mandihamza <mandihamza@gmail.com>
2018-12-09 15:44:56 -0700

    SEO updates

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Welcome to my portfolio!
 
-##Built with
+## Built with
 - [Gatsby](https://www.gatsbyjs.org/)
 - [Cara starter](https://portfolio-cara.netlify.com/)
 - [Netlify](https://www.netlify.com)
--- config/website.js
+++ config/website.js
@@ -3,16 +3,16 @@ const tailwind = require('../tailwind');
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Cara - Gatsby Starter Portfolio', // Navigation and Site Title
-  siteTitleAlt: 'Cara', // Alternative Site title for SEO
-  siteUrl: 'https://portfolio-cara.netlify.com', // Domain of your site. No trailing slash!
+  siteTitle: 'Mandi Hamza | Portfolio', // Navigation and Site Title
+  siteTitleAlt: 'Mandi Hamza | Portfolio', // Alternative Site title for SEO
+  siteUrl: 'https://www.technicallymandi.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
-  siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
+  siteDescription: 'I\'m a self-taught developer with experience in e-commerce marketing & sponsorship development.',
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@cara', // Twitter Username
-  ogSiteName: 'cara', // Facebook Site Name
+  userTwitter: '@mandihamza', // Twitter Username
+  ogSiteName: 'Mandi Hamza - Developer | Portfolio', // Facebook Site Name
   ogLanguage: 'en_US', // Facebook Language
 
   // Manifest and Progress color
